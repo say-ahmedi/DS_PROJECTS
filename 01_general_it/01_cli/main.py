a1e2d5f16e252265410012d64865b96193abcaf4
Bedevi <seidahmedrashid@gmail.com>
2026-09-21 16:05:30 +0500

    refactor: update main.py and implement CLI
    Fix code issues and finalize project structure.

--- 01_general_it/01_cli/main.py
+++ 01_general_it/01_cli/main.py
@@ -1,8 +1,38 @@
+from commands import Commands
+
 class DevFlow:
-    def __init__(self):
-        self.proj_storage = {}
-        self.individual_proj = {}
-        
-    def start_app(self):
-        pass
+    introduction = """
+    ====================================
+            DevFlow CLI
+    ====================================
+    Welcome to DevFlow.
     
+    Type "help" to see available commands.
+    """
+    help_text = '''
+Available commands:
+
+create              Create a new project
+list                List all projects
+show <id>           Show one project
+update <id>         Update a project
+delete <id>         Delete a project
+help                Show available commands
+exit                Close DevFlow'''
+    def __init__(self):
+        pass
+        
+
+
+
+    def start_app(self):
+        is_running = True 
+        while is_running:
+            print(self.introduction)
+            user_input = ("devflow>")
+            if user_input == 'help':
+                print(DevFlow.help_text)
+
+
+devflow = DevFlow()
+devflow.start_app()
