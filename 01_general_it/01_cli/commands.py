a8c76f3f809860fb5d8844f8c94d8f5e085858ea
Bedevi <seidahmedrashid@gmail.com>
2026-09-25 00:28:15 +0500

    refactor: organize Commands class
    Fix issues in the file and project structure.

--- 01_general_it/01_cli/commands.py
+++ 01_general_it/01_cli/commands.py
@@ -1,10 +1,24 @@
 class Commands:
+    n_p = 0
+    n_pp = 0
+    proj = {}
+    
     def __init__(self):
         pass
     
     def create_project(self):
-        pass
-    
+        name = input('Project Name: ')
+        description = input('Description: ')
+        status = input('Status [planned/active/completed]: ')
+        n_p += 1
+        n_pp += 1
+        self.proj[n_pp] = {
+            'name':name,
+            'description':description,
+            'status':status
+        }
+        print(f'✓ Project #{n_p} created successfully.') 
+        
     def list_project(self):
         pass
     def show_project(self):
